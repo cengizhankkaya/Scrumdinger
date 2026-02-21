d5c489f0c9fa72516c6cbc224f2c22b65da5a6c0
Cengizhan Kaya <92298156+cengizhankkaya@users.noreply.github.com>
2026-02-21 22:53:44 +0300

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by cengizhan kaya on 21.02.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by cengizhan kaya on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
