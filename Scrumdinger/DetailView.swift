74fdca581eac029ea7e4468307acfcadd6b99438
Cengizhan Kaya <cengizhan.kaya.1453@gmail.com>
2026-03-15 22:22:19 +0300

    code review

--- Scrumdinger/DetailView.swift
+++ Scrumdinger/DetailView.swift
@@ -1,43 +1,47 @@
-//
-//  DetailView.swift
-//  Scrumdinger
-//
-//  Created by cengizhan kaya on 22.02.2026.
-//
-
 import SwiftUI
 import ThemeKit
 
+
+
 struct DetailView: View {
     let scrum: DailyScrum
-    
+
+
     var body: some View {
         List {
             Section(header: Text("Meeting Info")) {
-                Label("Star Meeting", systemImage: "timer")
-                    .font(.headline)
-                    .foregroundColor(.accentColor)
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .font(.headline)
+                        .foregroundColor(.accentColor)
+                }
                 HStack {
                     Label("Length", systemImage: "clock")
                     Spacer()
                     Text("\(scrum.lengthInMinutes) minutes")
                 }
+                .accessibilityElement(children: .combine)
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .foregroundColor(scrum.theme.accentColor)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                }
+                .accessibilityElement(children: .combine)
             }
-            .accessibilityElement(children: .combine)
-            HStack {
-                Label("Theme", systemImage: "paintpalette")
-                Spacer()
-                Text(scrum.theme.name)
-                    .padding(4)
-                    .foregroundColor(scrum.theme.accentColor)
-                    .background(scrum.theme.mainColor)
-                    .cornerRadius(4)
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label(attendee.name, systemImage: "person")
+                }
             }
-            .accessibilityElement(children: .combine)
-            
         }
+        .navigationTitle(scrum.title)
     }
 }
+
 
 #Preview {
     NavigationStack {
