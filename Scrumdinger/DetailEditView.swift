5c559182fb2dbb5f4666259973a04ea2d29c67b3
Cengizhan Kaya <cengizhan.kaya.1453@gmail.com>
2026-03-27 14:14:19 +0300

    implement DetailEditView and update DetailView for editing scrums

---
+++ Scrumdinger/DetailEditView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailEditView.swift
+//  Scrumdinger
+//
+//  Created by cengizhan kaya on 26.03.2026.
+//
+
+import SwiftUI
+
+struct DetailEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailEditView()
+}
